6493f07614fc004214cea597413a2ad2d356f402
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2026-08-07 02:02:35 +0530

    Add files via upload

---
+++ test_api.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_version():
+    response = client.get("/version")
+    assert response.status_code == 200
